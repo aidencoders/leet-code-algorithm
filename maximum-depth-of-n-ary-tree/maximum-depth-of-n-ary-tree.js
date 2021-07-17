dd08658e82b50f33f9a4c09f12f9afc7b99810fe
Taewan Kim <77876142+aidencoders@users.noreply.github.com>
2021-07-17 13:43:51 +0900

    Time: 92 ms (60.63%), Space: 41.5 MB (66.97%) - LeetHub

--- maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.js
+++ maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.js
@@ -11,23 +11,33 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if(!root) {
-        return 0;
-    }
+//     if(!root) {
+//         return 0;
+//     }
     
-    let result = 1;
+//     let result = 1;
     
-    function helper(node, depth = 1) {
-        if(!node.children.length) {
-            result = Math.max(result, depth);
-            return;
-        }
+//     function helper(node, depth = 1) {
+//         if(!node.children.length) {
+//             result = Math.max(result, depth);
+//             return;
+//         }
         
-        for(let i = 0; i < node.children.length; ++i) {
-            helper(node.children[i], depth + 1);    
-        }
-    }
+//         for(let i = 0; i < node.children.length; ++i) {
+//             helper(node.children[i], depth + 1);    
+//         }
+//     }
     
-    helper(root);
-    return result;
+//     helper(root);
+    
+//     return result;
+    if (!root) return 0;
+
+  let max = 0;
+
+  for (const child of root.children) {
+    max = Math.max(max, maxDepth(child));
+  }
+
+  return 1 + max;
 };
